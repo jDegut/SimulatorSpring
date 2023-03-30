bbe865ce4f00b58550c928dcf41e713216aa2239
Julian <101513950+jDegut@users.noreply.github.com>
2023-03-30 12:12:57 +0200

    Repo init

---
+++ src/main/java/fr/polytech/simulatorspring/SimulatorSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.simulatorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/polytech/simulatorspring/SimulatorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.simulatorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatorSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
