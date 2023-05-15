49948c0dda89dd8c3ada7197ec621f5b6504fb1f
jdegu <jdegut@hotmail.fr>
2023-05-15 09:58:31 +0200

    test v2

--- src/main/java/fr/polytech/simulatorspring/controller/HomeController.java
+++ src/main/java/fr/polytech/simulatorspring/controller/HomeController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
-
-
 	@GetMapping
 	public String home() {
 		return "Hello World!";
